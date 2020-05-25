43992ecf58797a43ad8347a645cbe05fbfc30795
gisellerocha <gijob.97@gmail.com>
2020-05-25 00:35:36 -0300

    :zap: Screen login change to CadastroUsuario and Dashboard (mapping only)

--- app/src/main/java/br/saojudas/mobile/healthcare/ui/activity/LoginActivity.java
+++ app/src/main/java/br/saojudas/mobile/healthcare/ui/activity/LoginActivity.java
@@ -3,10 +3,13 @@ package br.saojudas.mobile.healthcare.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.w3c.dom.Text;
 
@@ -18,9 +21,10 @@ import br.saojudas.mobile.healthcare.ui.adapter.CadastroUsuarioAdapter;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TITULO_APPBAR_LOGIN = "LoginActivity";
-    private EditText campoLoginLogin;
-    private EditText campoSenhaLogin;
-    private TextView txtCadastro;
+    private EditText editLogin;
+    private EditText editSenha;
+    private Button btnEntrar;
+    private Button btnCadastro;
     private RoomUsuarioDAO dao;
     private CadastroUsuario cadastroUsuario;
     private CadastroUsuarioAdapter cadastroUsuarioAdapter;
@@ -33,18 +37,29 @@ public class LoginActivity extends AppCompatActivity {
         dao = database.getRoomUsuarioDAO();
         setTitle(TITULO_APPBAR_LOGIN);
 
-        campoLoginLogin = findViewById(R.id.campoLoginLogin);
-        campoSenhaLogin = findViewById(R.id.campoSenhaLogin);
-        txtCadastro = findViewById(R.id.txtCadastro);
+        editLogin = findViewById(R.id.editLogin);
+        editSenha = findViewById(R.id.editSenha);
+        btnCadastro = findViewById(R.id.btnCadastro);
         abreCadastro();
+        abreDashboard();
     }
 
     private void abreCadastro() {
-        TextView txtCadastro = findViewById(R.id.txtCadastro);
-        txtCadastro.setOnClickListener(new View.OnClickListener() {
+        Button btnCadastro = findViewById(R.id.btnCadastro);
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+                Intent it = new Intent(LoginActivity.this, CadastroUsuario.class);
+                startActivity(it);
+            }
+        });
+    }
+    private void abreDashboard() {
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(it);
             }
         });
